69ce1eb4c9f96ca001ae7f813bb34d60fab20b85
Giorgos Balis <grgbls647@gmail.com>
2024-06-14 00:47:34 +0300

    Update Receipt.py

--- Receipt.py
+++ Receipt.py
@@ -16,12 +16,3 @@ def receipt():
         print(str(items[i])+ ":" + (25-len(str(items[i])))*" "+str(prices[i]))
     print ("="*30)
     print("total:"+20*" "+str(total))
-
-if __name__ == "__main__":
-    add_items("red bull", 1.5)
-    add_items("monster", 1.2)
-    add_items("hell", 0.9)
-    add_items("water", 0.5)
-
-    receipt()
-    
